55eb4ec01120d311a427bb58d78b03ca735be551
naylafauziah <naylafauziah6514@gmail.com>
2023-04-05 20:32:48 +0700

    latihan modul 10

---
+++ src/main/java/nayla050423/StudentRecordExample.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nayla050423;
+
+/**
+ *
+ * @author TUF Gaming
+ */
+public class StudentRecordExample {
+    public static void main(String[] args) {
+        StudentRecord annaRecord = new StudentRecord();
+        StudentRecord crisRecord = new StudentRecord();
+        
+        annaRecord.setName("Anna");
+        annaRecord.setAddress("Padang");
+        annaRecord.setAge(20);
+        annaRecord.setMathGrade(85);
+        annaRecord.setEnglishGrade(78);
+        annaRecord.setScienceGrade(90);
+        
+        //Tampil data anna
+        System.out.println("Nama     : "+annaRecord.getName());
+        System.out.println("Alamat   : "+annaRecord.getAddress());
+        System.out.println("Umur     : "+annaRecord.getAge());
+        System.out.println("Nilai Matematika   : "+annaRecord.getMathGrade());
+        System.out.println("Nilai B. Inggris   : "+annaRecord.getEnglishGrade());
+        System.out.println("Nilai Sains        : "+annaRecord.getScienceGrade());
+        System.out.println("Rata-Rata          : "+annaRecord.getAverage());
+        
+        crisRecord.setName("Cris");
+        crisRecord.setAddress("Kanada");
+        crisRecord.setAge(22);
+        crisRecord.setMathGrade(80);
+        crisRecord.setEnglishGrade(99);
+        crisRecord.setScienceGrade(85);
+        
+        //Tampil data cris
+        System.out.println("\nNama     : "+crisRecord.getName());
+        System.out.println("Alamat   : "+crisRecord.getAddress());
+        System.out.println("Umur     : "+crisRecord.getAge());
+        System.out.println("Nilai Matematika   : "+crisRecord.getMathGrade());
+        System.out.println("Nilai B. Inggris   : "+crisRecord.getEnglishGrade());
+        System.out.println("Nilai Sains        : "+crisRecord.getScienceGrade());
+        System.out.println("Rata-Rata          : "+crisRecord.getAverage());
+        
+        System.out.println("");
+        StudentRecord aliRecord = new StudentRecord();
+        aliRecord.setName("Ali");
+        aliRecord.setAddress("Belgia");
+        aliRecord.setAge(18);
+        
+        aliRecord.print("");
+        aliRecord.print(95,80,90);
+        System.out.println("Rata-Rata     : "+aliRecord.getAverage());
+        
+        System.out.println("Count :"+StudentRecord.getStudentCount());
+    }
+}
